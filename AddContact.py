64314e054d0eec6f0378ad6e615c9490e58df005
Сергей <34037917+SergeD3@users.noreply.github.com>
2021-02-21 19:40:39 +0300

    доработка методов + замена и передача параметров в методы.

--- AddContact.py
+++ AddContact.py
@@ -14,9 +14,11 @@ class AddContact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.open_homepage(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_add_contact(wd)
-        self.fill_contact_form(wd)
+        self.fill_contact_form(wd, middle_name="middle name 1", first_name="name 1", last_name="last name 1", user_nick="nick 1", user_title="title 1", user_company="wrum",
+                               user_adress="lazer 5, Moscow", user_home="784763587643", user_mob="746358746357", user_email="weuyrt@iwer.ri", user_bday="17", user_bmonth="October",
+                               user_byear="1980", user_group="new group2", user_addr2="lazer 5, Russian Federation", user_phone2="rgegregreg", user_note="notes from user")
         self.contact_lv(wd)
         self.logout(wd)
 
@@ -28,73 +30,75 @@ class AddContact(unittest.TestCase):
         # переходим на форму списка контактов - убеждаемся, что контакт создался
         wd.find_element_by_link_text("home").click()
 
-    def fill_contact_form(self, wd):
+    def fill_contact_form(self, wd, middle_name, first_name, last_name, user_nick, user_title, user_company,
+                          user_adress, user_home, user_mob, user_email, user_bday, user_bmonth, user_byear, user_group,
+                          user_addr2, user_phone2, user_note):
         # заполняем поля
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("name 1")
+        wd.find_element_by_name("firstname").send_keys(first_name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("middle name 1")
+        wd.find_element_by_name("middlename").send_keys(middle_name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("last name 1")
+        wd.find_element_by_name("lastname").send_keys(last_name)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("nick 1")
+        wd.find_element_by_name("nickname").send_keys(user_nick)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("title 1")
+        wd.find_element_by_name("title").send_keys(user_title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("wrum")
+        wd.find_element_by_name("company").send_keys(user_company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("lazer 5, Moscow")
+        wd.find_element_by_name("address").send_keys(user_adress)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("784763587643")
+        wd.find_element_by_name("home").send_keys(user_home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("746358746357")
+        wd.find_element_by_name("mobile").send_keys(user_mob)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("weuyrt@iwer.ri")
+        wd.find_element_by_name("email").send_keys(user_email)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("17")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(user_bday)
         wd.find_element_by_xpath("//option[@value='17']").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("October")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(user_bmonth)
         wd.find_element_by_xpath("//option[@value='October']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1980")
+        wd.find_element_by_name("byear").send_keys(user_byear)
         wd.find_element_by_name("new_group").click()
-        Select(wd.find_element_by_name("new_group")).select_by_visible_text("new group2")
+        Select(wd.find_element_by_name("new_group")).select_by_visible_text(user_group)
         wd.find_element_by_xpath("(//option[@value='10'])[3]").click()
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys("lazer 5, Russian Federation")
+        wd.find_element_by_name("address2").send_keys(user_addr2)
         wd.find_element_by_name("phone2").click()
         wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys("rgegregreg")
+        wd.find_element_by_name("phone2").send_keys(user_phone2)
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("wefewfwefe")
+        wd.find_element_by_name("notes").send_keys(user_note)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def open_add_contact(self, wd):
         # переходим в раздел добавления контакта
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         # представляемся системе
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_homepage(self, wd):
